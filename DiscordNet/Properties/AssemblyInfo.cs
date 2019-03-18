60e479f86914d98f9a70558de5f83bd500713d00
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2019-03-18 09:50:39 +0100

    Compiled and tested

--- DiscordNet/Properties/AssemblyInfo.cs
+++ DiscordNet/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.42.7016.15788")]
-[assembly: AssemblyFileVersion("1.42.7016.15788")]
+[assembly: AssemblyVersion("1.43.7016.15897")]
+[assembly: AssemblyFileVersion("1.43.7016.15897")]
